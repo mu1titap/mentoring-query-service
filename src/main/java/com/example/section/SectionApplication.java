ceabfb3f39f0f0ebb7fd28f6dc62efe9d223b9e8
Jung-Hey <gjgmlwjd9@gmail.com>
2024-12-09 20:37:46 +0900

    test

--- src/main/java/com/example/section/SectionApplication.java
+++ src/main/java/com/example/section/SectionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @SpringBootApplication
 @EnableMongoAuditing
 public class SectionApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(SectionApplication.class, args);
 	}
