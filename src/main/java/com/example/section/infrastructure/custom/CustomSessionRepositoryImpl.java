c636d27902f86cc7daff251a2533ef0e360d54c2
Jung-Hey <gjgmlwjd9@gmail.com>
2024-12-13 13:34:21 +0900

    findAllByMentoringUuidAndDeadlineDateV2 레포지토리 로직 수정

--- src/main/java/com/example/section/infrastructure/custom/CustomSessionRepositoryImpl.java
+++ src/main/java/com/example/section/infrastructure/custom/CustomSessionRepositoryImpl.java
@@ -79,6 +79,7 @@ public class CustomSessionRepositoryImpl implements CustomSessionRepository {
     public List<MentoringSessionResponseDto> findAllByMentoringUuidAndDeadlineDateV2(String mentoringUuid, String userUuid) {
 
         Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.unwind("sessionUsers", true),
                 Aggregation.match(Criteria.where("mentoringUuid").is(mentoringUuid)
                         .and("isDeleted").is(false)
                         .and("deadlineDate").gte(LocalDate.now())),
@@ -91,7 +92,7 @@ public class CustomSessionRepositoryImpl implements CustomSessionRepository {
                         ).build(),
 
                 // sessionUsers 배열을 펼침
-                Aggregation.unwind("sessionUsers", true),
+
                 // sessionUsers를 배열로 다시 그룹화
                 Aggregation.group("sessionUuid", "mentoringUuid", "startDate", "endDate", "startTime", "endTime",
                                 "deadlineDate", "minHeadCount", "maxHeadCount", "nowHeadCount", "isParticipating", "price", "isClosed")
